64d1e14ddf01ebb236d327079df5b5c36c04e37d
IzzyH787 <isabel.hampton@mail.bcu.ac.uk>
2025-02-12 11:24:09 +0000

    Drone tilts left/right on input

--- Assets/Scripts/DroneMovement.cs
+++ Assets/Scripts/DroneMovement.cs
@@ -7,6 +7,7 @@ public class DroneMovement : MonoBehaviour
 {
     public float tiltAngle; //in degrees
     public float speed;
+    public float tiltStep;
 
     float upDownInput = 0.0f;
     float leftRightInput = 0.0f;
@@ -17,10 +18,36 @@ public class DroneMovement : MonoBehaviour
     {
         //move drone up/down
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + (upDownInput * speed), gameObject.transform.position.z);
+        Debug.Log(gameObject.transform.rotation.z);
         //move drone left/right
         gameObject.transform.position = new Vector3(gameObject.transform.position.x + (leftRightInput * speed), gameObject.transform.position.y, gameObject.transform.position.z);
         //rotate drone left/right
-        gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z + (tiltAngle * leftRightInput), 1.0f );
+        //if not fully tilted and left right input is true
+        //(gameObject.transform.rotation.z > -tiltAngle && leftRightInput < 0)  || (gameObject.transform.rotation.z < tiltAngle && leftRightInput > 0)
+        //check if input and not fully rotated either way
+        if (leftRightInput != 0 && gameObject.transform.rotation.z < tiltAngle && gameObject.transform.rotation.z > -tiltAngle)
+        {
+            gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z - (tiltStep * leftRightInput), 1.0f);
+
+        }
+        
+        
+        //if drone is tilted but left/right input is not active
+        else if (gameObject.transform.rotation.z != 0 && leftRightInput == 0)
+        {
+            //make sur etilt is corrected in correct direction
+            if (gameObject.transform.rotation.z < 0)
+            {
+                gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z + tiltStep, 1.0f);
+
+            }
+            else
+            {
+                gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z - tiltStep, 1.0f);
+
+            }
+
+        }
         //move drone forward/back
 
         //rotate drone forward/back
