92e9013ea13fc118ab790fddd1dc55d11fe9035b
IzzyH787 <isabel.hampton@mail.bcu.ac.uk>
2025-02-12 01:00:38 +0000

    Linked inputs to functions

---
+++ Assets/Scripts/DroneMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnUpDown()
+    {
+
+    }
+    public void OnLeftRight()
+    {
+
+    }
+    public void OnForwardBack()
+    {
+
+    }
+    public void OnRotate()
+    {
+
+    }
+}
