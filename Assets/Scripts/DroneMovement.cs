8a2e4de9b607d9ed423cc08862adc68704d07f96
IzzyH787 <isabel.hampton@mail.bcu.ac.uk>
2025-02-12 01:19:39 +0000

    Drone has up/down and left/right movement

--- Assets/Scripts/DroneMovement.cs
+++ Assets/Scripts/DroneMovement.cs
@@ -1,35 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DroneMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float tiltAngle; //in degrees
+    public float speed;
+
+    float upDownInput = 0.0f;
+    float leftRightInput = 0.0f;
+    float forwardBackInput = 0.0f;
+    float rotateInput = 0.0f;
+
+    private void Update()
     {
-        
+        //move drone up/down
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + (upDownInput * speed), gameObject.transform.position.z);
+        //move drone left/right
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + (leftRightInput * speed), gameObject.transform.position.y, gameObject.transform.position.z);
+        //rotate drone left/right
+        gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z + (tiltAngle * leftRightInput), 1.0f );
+        //move drone forward/back
+
+        //rotate drone forward/back
+
+        //rotate drone
+
+        //rotate camera
+
     }
-
-    // Update is called once per frame
-    void Update()
+    public void OnUpDown(InputAction.CallbackContext ctx)
     {
-        
+        upDownInput = ctx.ReadValue<float>(); //reads direction 
+   }
+    public void OnLeftRight(InputAction.CallbackContext ctx)
+    {
+        leftRightInput = ctx.ReadValue<float>(); //reads direction
     }
-
-    public void OnUpDown()
+    public void OnForwardBack(InputAction.CallbackContext ctx)
     {
-
+        forwardBackInput = ctx.ReadValue<float>(); //reads direction
     }
-    public void OnLeftRight()
+    public void OnRotate(InputAction.CallbackContext ctx)
     {
-
-    }
-    public void OnForwardBack()
-    {
-
-    }
-    public void OnRotate()
-    {
-
+        rotateInput = ctx.ReadValue<float>(); //reads direction
     }
 }
