2aa8064f5cb23efd3a32b3708d7a68b383ebdf8b
IzzyH787 <isabel.hampton@mail.bcu.ac.uk>
2025-02-28 14:59:55 +0000

    Set up basic UI functionality in IHA_TestLevel2

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -10,6 +10,7 @@ public class LevelManager : MonoBehaviour
     public float timer = 0;
 
     //ui screens
+    public GameObject sceneStartPanel;
     public GameObject startScreen;
     public GameObject levelSelector;
     public GameObject hud;
@@ -23,22 +24,23 @@ public class LevelManager : MonoBehaviour
     public GameObject sceneStartBtn;
     public GameObject playBtn;
     public GameObject level1Btn;
-    public GameObject settingsBtn;
+    public GameObject pauseBtn;
     public GameObject resumeBtn;
     public GameObject continueBtn;
+    public GameObject closeSettingsBtn;
 
     // Start is called before the first frame update
     void Start()
     {
-        sceneStartBtn = playBtn;
-        //show start screen
-        startScreen.SetActive(true);
-        //hide all other uis
+        //hide all  uis
+        startScreen.SetActive(false);
         levelSelector.SetActive(false);
         hud.SetActive(false);
         pausePanel.SetActive(false);
         settingsPanel.SetActive(false);
         levelOverScreen.SetActive(false);
+        //show scenes start panel
+        sceneStartPanel.SetActive(true);
         //auto selectplay button
         eventSystem.firstSelectedGameObject = sceneStartBtn;
     }
@@ -74,16 +76,14 @@ public class LevelManager : MonoBehaviour
     {
         Time.timeScale = 1; //make timer continue
         //load level 1
-
+        SceneManager.LoadScene(1);
         //set default button to pause
-        eventSystem.firstSelectedGameObject = settingsBtn;
     }
     public void OnLevel2()
     {
         Time.timeScale = 1; //make timer continue
         //load level 2
-
-        //set default button to pause
+        SceneManager.LoadScene(2);
     }
     public void OnLevel3()
     {
@@ -96,33 +96,49 @@ public class LevelManager : MonoBehaviour
     public void OnPause()
     {
         Time.timeScale = 0; // stop timer while paused
+        //hide hud
+        hud.SetActive(false);
         //show pause panel 
-
+        pausePanel.SetActive(true);
         //set default button to resume
-
+        eventSystem.firstSelectedGameObject = resumeBtn;
 
     }
     public void OnResume()
     {
         Time.timeScale = 1; //make timer continue
         //hide pause panel
-
+        pausePanel.SetActive(false);
+        //show hud
+        hud.SetActive(true);
         //set dault button to pause
+        eventSystem.firstSelectedGameObject = pauseBtn;
     }
     public void OnSettings()
     {
+        //hide paused panel
+        pausePanel.SetActive(false);
         //open setting menu
-
-        //set panel to active
-
-        //set default button
+        settingsPanel.SetActive(true);
+        //set default button to quit
+        eventSystem.firstSelectedGameObject = closeSettingsBtn;
     }
     public void OnEndLevel()
     {
-        //return to level selector
-
+        //hide paused menu
+        pausePanel.SetActive(false);
         //hide hud
-
-        //set default button
+        hud.SetActive(false);
+        //return to level selector
+        OnPlay();
+    }
+    public void OnCloseSetting()
+    {
+        //hide settings
+        settingsPanel.SetActive(false);
+        //show paused panel
+        pausePanel.SetActive(true);
+        //set deault button to resume
+        eventSystem.firstSelectedGameObject = resumeBtn;
     }
 }
