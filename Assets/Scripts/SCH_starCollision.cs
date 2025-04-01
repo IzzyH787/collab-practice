d85ef5997fccd5d8731b4d2afdf2371d311b2352
Sum-Yuet-Chan <146599172+Sum-Yuet-Chan@users.noreply.github.com>
2025-04-02 00:30:28 +0100

    better buttons
    2-4-25

---
+++ Assets/Scripts/SCH_starCollision.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCH_starCollision : MonoBehaviour
+{
+    public GameObject droneObj; //The drone object
+    private Rigidbody droneRB; //Rigidbody of the drone
+    private bool achievementUnlocked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        droneRB = droneObj.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!achievementUnlocked)
+        {
+            if (collision.gameObject == droneObj)
+            {
+                Debug.Log("You became the star!");
+                achievementUnlocked = true;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
